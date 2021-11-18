c830076169f5c79955072053a8fc2c9bfc77a1e4
theflyingzamboni <theaerialzamboni@gmail.com>
2021-11-17 19:41:47 -0500

    Reworked SetFileValue to handle adding LODXA03.XA and MIX.DA, added new globals

--- Globals.cs
+++ Globals.cs
@@ -15,5 +15,35 @@ namespace LodmodsDM
             { "ITA", new string[] { "SCES_030.46", "SCES_130.46", "SCES_230.46", "SCES_330.46" } },
             { "SPA", new string[] { "SCES_030.47", "SCES_130.47", "SCES_230.47", "SCES_330.47" } },
         };
+
+        public static readonly string[] UNIVERSAL_FILES = new string[]
+        {
+            "OHTA/MCX/DABAS.BIN", "OVL/BTTL.OV_", "OVL/S_BTLD.OV_", "OVL/S_EFFE.OV_",
+            "OVL/S_INIT.OV_", "OVL/S_ITEM.OV_", "OVL/S_STRM.OV_", "OVL/SMAP.OV_", "OVL/TEMP.OV_", 
+            "OVL/TTLE.OV_", "OVL/WMAP.OV_", "SECT/DRGN0.BIN", "SECT/DRGN1.BIN", "SIM/MES.MVB", 
+            "SUBMAP/NEWROOT.RDT", "XA/LODXA00.XA", "XA/LODXA01.XA", "XA/LODXA02.XA"
+        };
+
+        public static readonly string[] COMPLETE_FILE_LIST = new string[]
+        {
+            "OHTA/MCX/DABAS.BIN", "OVL/BTTL.OV_", "OVL/S_BTLD.OV_", "OVL/S_EFFE.OV_",
+            "OVL/S_INIT.OV_", "OVL/S_ITEM.OV_", "OVL/S_STRM.OV_", "OVL/SMAP.OV_", "OVL/TEMP.OV_",
+            "OVL/TTLE.OV_", "OVL/WMAP.OV_", "SECT/DRGN0.BIN", "SECT/DRGN1.BIN", "SIM/MES.MVB",
+            "SUBMAP/NEWROOT.RDT", "XA/LODXA00.XA", "XA/LODXA01.XA", "XA/LODXA02.XA",
+            "SCUS_944.91", "SCUS_945.84", "SCUS_945.85", "SCUS_945.86",
+            "SCPS_101.19", "SCPS_101.20", "SCPS_101.21", "SCPS_101.22",
+            "SCPS_454.61", "SCPS_454.62", "SCPS_454.63", "SCPS_454.64",
+            "SCES_030.43", "SCES_130.43", "SCES_230.43", "SCES_330.43",
+            "SCES_030.44", "SCES_130.44", "SCES_230.44", "SCES_330.44",
+            "SCES_030.45", "SCES_130.45", "SCES_230.45", "SCES_330.45",
+            "SCES_030.46", "SCES_130.46", "SCES_230.46", "SCES_330.46",
+            "SCES_030.47", "SCES_130.47", "SCES_230.47", "SCES_330.47",
+            "SECT/DRGN21.BIN", "STR/DEMO2.IKI", "STR/DEMOH.IKI", "STR/OPENH.IKI", 
+            "STR/WAR1H.IKI", "SECT/DRGN22.BIN", "STR/GOAST.IKI", "STR/ROZEH.IKI", 
+            "STR/TVRH.IKI", "SECT/DRGN23.BIN", "STR/BLACKH.IKI", "STR/DEIASH.IKI",
+            "STR/DENIN.IKI", "STR/DENIN2.IKI", "STR/DRAGON1.IKI", "STR/DRAGON2.IKI",
+            "STR/TREEH.IKI", "STR/WAR2H.IKI", "SECT/DRGN24.BIN", "STR/ENDING1H.IKI",
+            "STR/ENDING2H.IKI", "STR/MOONH.IKI", "XA/LODXA03.XA", "SYSTEM.CNF"
+        };
     }
 }
--- MainConfigHandler.cs
+++ MainConfigHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using static LodmodsDM.Globals;
 
 namespace LodmodsDM
 {
@@ -40,12 +41,16 @@ namespace LodmodsDM
 
     public class DiscFileConfig
     {
+        public string Region { get; }
+        public string DiscName { get; }
         public string DiscFileName { get; set; }
 
         public Dictionary<string, bool> DiscFileDict { get; private set; } = new Dictionary<string, bool>();
 
-        public DiscFileConfig(string region, string discName, Dictionary<string, string[]> regionCodes)
+        public DiscFileConfig(string region, string discName)
         {
+            Region = region;
+
             switch (discName)
             {
                 case "Disc 1":
@@ -56,11 +61,11 @@ namespace LodmodsDM
                         { "STR/DEMOH.IKI", false },
                         { "STR/OPENH.IKI", false },
                         { "STR/WAR1H.IKI", false },
-                        { regionCodes[region][0], false },
+                        { REGION_CODES[region][0], false },
                         { "SYSTEM.CNF", false }
                     };
-                    this.AddLODXA03(region, discName);
-                    this.AddMixDA(region, discName);
+                    this.SetFileValue("XA/LODXA03.XA", false);
+                    this.SetFileValue("DA/MIX.DA", false);
                     break;
                 case "Disc 2":
                     DiscFileDict = new Dictionary<string, bool>()
@@ -69,11 +74,11 @@ namespace LodmodsDM
                         { "STR/GOAST.IKI", false },
                         { "STR/ROZEH.IKI", false },
                         { "STR/TVRH.IKI", false },
-                        { regionCodes[region][1] , false },
+                        { REGION_CODES[region][1] , false },
                         { "SYSTEM.CNF", false },
                     };
-                    this.AddLODXA03(region, discName);
-                    this.AddMixDA(region, discName);
+                    this.SetFileValue("XA/LODXA03.XA", false);
+                    this.SetFileValue("DA/MIX.DA", false);
                     break;
                 case "Disc 3":
                     DiscFileDict = new Dictionary<string, bool>()
@@ -87,11 +92,11 @@ namespace LodmodsDM
                         { "STR/DRAGON2.IKI", false },
                         { "STR/TREEH.IKI", false },
                         { "STR/WAR2H.IKI", false },
-                        { regionCodes[region][2], false },
+                        { REGION_CODES[region][2], false },
                         { "SYSTEM.CNF", false },
                     };
-                    this.AddLODXA03(region, discName);
-                    this.AddMixDA(region, discName);
+                    this.SetFileValue("XA/LODXA03.XA", false);
+                    this.SetFileValue("DA/MIX.DA", false);
                     break;
                 case "Disc 4":
                     DiscFileDict = new Dictionary<string, bool>()
@@ -101,6 +106,7 @@ namespace LodmodsDM
                         { "STR/ENDING2H.IKI", false },
                         { "STR/MOONH.IKI", false },
                         { "XA/LODXA03.XA", false },
+                        { REGION_CODES[region][3], false },
                         { "SYSTEM.CNF", false }
                     };
                     break;
@@ -137,6 +143,13 @@ namespace LodmodsDM
             DiscFileName = discFileName;
         }
 
+        // If I need to combine them I guess. Not sure I'll need these, but here they are
+        public DiscFileConfig(string region, string discName, string discFileName) : 
+            this(region, discName)
+        {
+            DiscFileName = discFileName;
+        }
+
         public bool GetFileValue(string key)
         {
             bool value;
@@ -146,40 +159,27 @@ namespace LodmodsDM
 
         public void SetFileValue(string key, bool value)
         {
-            DiscFileDict[key] = value;
-        }
-
-        private void AddLODXA03(string region, string discName)
-        {
+            key = key.ToUpper();
             string[] jpCodes = new string[] { "JP1", "JP2" };
-            if (!Array.Exists(jpCodes, r => r == region))
+
+            if (!Array.Exists(COMPLETE_FILE_LIST, k => k == key)) {
+                throw new ArgumentException($"{key} is not a valid file name.");
+            } else if (key == "XA/LODXA03.XA")
             {
-                this.SetFileValue("XA/LODXA03.XA", false);
-            }
-        }
-        private void AddMixDA(string region, string discName)
-        {
-            if (Array.Exists(new string[] { "JP1", "JP2" }, r => r == region) 
-                && Array.Exists(new string[] { "Disc 1", "Disc 2", "Disc 3" }, d => d == discName))
+                if (!Array.Exists(jpCodes, r => r == Region)) { DiscFileDict[key] = value; }
+            } else if (key == "DA/MIX.DA")
             {
-                this.SetFileValue("DA/MIX.DA", false);
-            }
+                if (Array.Exists(new string[] { "JP1", "JP2" }, r => r == Region)
+                    && Array.Exists(new string[] { "Disc 1", "Disc 2", "Disc 3" }, d => d == DiscName))
+                {
+                    DiscFileDict[key] = value;
+                }
+            } else { DiscFileDict[key] = value; }
         }
     }
 
     public class GameRegionConfig
     {
-        private Dictionary<string, string[]> _regionCodes = new Dictionary<string, string[]>
-        {
-            { "USA", new string[] { "SCUS_944.91", "SCUS_945.84", "SCUS_945.85", "SCUS_945.86" } },
-            { "JP1", new string[] { "SCPS_101.19", "SCPS_101.20", "SCPS_101.21", "SCPS_101.22" } },
-            { "JP2", new string[] { "SCPS_454.61", "SCPS_454.62", "SCPS_454.63", "SCPS_454.64" } },
-            { "UK", new string[] { "SCES_030.43", "SCES_130.43", "SCES_230.43", "SCES_330.43" } },
-            { "FRA", new string[] { "SCES_030.44", "SCES_130.44", "SCES_230.44", "SCES_330.44" } },
-            { "GER", new string[] { "SCES_030.45", "SCES_130.45", "SCES_230.45", "SCES_330.45" } },
-            { "ITA", new string[] { "SCES_030.46", "SCES_130.46", "SCES_230.46", "SCES_330.46" } },
-            { "SPA", new string[] { "SCES_030.47", "SCES_130.47", "SCES_230.47", "SCES_330.47" } },
-        };
         //public Dictionary<string, string[]> RegionCodes => _regionCodes;
         //public string Region { get; private set; }
         public string DiscDir { get; set; }
@@ -195,7 +195,7 @@ namespace LodmodsDM
             for (int i = 0; i < 5; i++)
             {
                 string discName = discNameList[i];
-                GameDiscs.Add(discName, new DiscFileConfig(region, discName, _regionCodes));
+                GameDiscs.Add(discName, new DiscFileConfig(region, discName));
             }
         }
 
