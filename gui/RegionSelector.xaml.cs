db14b4db3337441a09aa5b7ab60b5ae1f7bfaf75
theflyingzamboni <theaerialzamboni@gmail.com>
2021-11-17 21:49:11 -0500

    Restructured project directory tree to incorporate GUI

---
+++ gui/RegionSelector.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LodmodsGUI
+{
+    public class ConfigConstructorInfo
+    {
+        public string ConfigName { get; set; }
+        public string ConfigType { get; set; }
+        public List<string> RegionList { get; set; }
+    }
+
+    public partial class RegionSelector : Window
+    {
+        public RegionSelector()
+        {
+            InitializeComponent();
+            base.DataContext = new ConfigConstructorInfo();
+        }
+
+        private void CancelRegionSelection(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void SetConfigName(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            // Create a MainConfig object, then open up form for customizing config
+            Close();
+        }
+    }
+}
