2d763b9d4907a3211482bf4377117d23639ac5ec
theflyingzamboni <theaerialzamboni@gmail.com>
2021-11-25 12:05:54 -0500

    Added Disc constructor, started ExtractDiscFile

--- core/DiscHandling/Disc.cs
+++ core/DiscHandling/Disc.cs
@@ -1,10 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace LodmodsDM
 {
     public class Disc
     {
+        static readonly byte[] _pvdHeader = new byte[] { 0x0, 0x0, 0x0, 0x9 };
+        public string FilePath { get; set; }
+        public byte[] SystemData { get; }
+        public PrimaryVolumeDescriptor PVD { get; set; }
+        public VolumeDescriptorSetTerminator VDST { get; }
+        public string ExtractedFileDirectory { get; set; }
+
+        public Disc(string filepath, string outputDirectory)
+        {
+            if (File.Exists(filepath)) FilePath = filepath; else throw new FileNotFoundException($"{filepath} does not exist.");
+            ExtractedFileDirectory = outputDirectory;
+            if (!Directory.Exists(ExtractedFileDirectory)) Directory.CreateDirectory(ExtractedFileDirectory);
+
+            using BinaryReader reader = new BinaryReader(File.OpenRead(FilePath));
+            {
+                if (!reader.ReadBytes(0xc).SequenceEqual(Globals.SYNC_PATTERN))
+                {
+                    throw new ArgumentException("Synchronization pattern not found. Incorrect file type.");
+                }
+                reader.BaseStream.Seek(0, SeekOrigin.Begin);
+
+                SystemData = reader.ReadBytes(0x9300);
+
+                reader.BaseStream.Seek(0x10, SeekOrigin.Current);
+                if (!reader.ReadBytes(0x4).SequenceEqual(_pvdHeader)) 
+                    throw new ArgumentException("Primary Volume descriptor header not found. Incorrect file type.");
+                reader.BaseStream.Seek(-0x14, SeekOrigin.Current);
+                PVD = new PrimaryVolumeDescriptor(reader);
+
+                // In place of creating proper DescriptorTerminator
+                reader.ReadBytes(0x930);
+                VDST = new VolumeDescriptorSetTerminator();
+            }
+        }
+
+        public static DirectoryTableEntry MatchPVDEntry(DirectoryTableEntry entry, string[] fileParts)
+        {
+            DirectoryTableEntry returnEntry = entry.Children.FirstOrDefault(
+                dirEntry => dirEntry.FileIdentifier.Split(";")[0] == fileParts[0]);
+
+            if (fileParts.Length > 1)
+            {
+                returnEntry = MatchPVDEntry(returnEntry, fileParts[1..]);
+                return returnEntry;
+            } else return returnEntry;
+        }
+
+        public void ExtractDiscFile(string filename)
+        {
+            string[] fileParts = filename.Split(Path.DirectorySeparatorChar);
+           
+            DirectoryTableEntry fileEntry = MatchPVDEntry(PVD.Root, fileParts);
+
+            // Get necessary information from fileEntry
+        }
     }
 }
