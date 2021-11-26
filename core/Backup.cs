2d24b701247a8c801f7de96c850e38d7211df058
theflyingzamboni <theaerialzamboni@gmail.com>
2021-11-26 12:04:08 -0500

    Added file backup method

---
+++ core/Backup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace LodmodsDM
+{
+    public class Backup
+    {
+        public static void BackupFile(string inputFile, bool restoreFromBackup=false, bool hideOutput=false)
+        {
+            string inputBackup = string.Join(".", new string[] { inputFile, "orig" });
+
+            if (!File.Exists(inputBackup))
+            {
+                if (!hideOutput) Console.WriteLine($"Backing up {inputFile}.");
+                File.Copy(inputFile, inputBackup);
+            } else if (restoreFromBackup)
+            {
+                if (!hideOutput) Console.WriteLine($"Restoring {inputFile} from backup}.");
+                File.Copy(inputBackup, inputFile, true);
+            }
+        }
+    }
+}
